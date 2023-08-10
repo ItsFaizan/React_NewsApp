84486d4cbfc8c12e2ffb2861ecb46a388438437a
ItsFaizan <faizanjavid71@gmail.com>
2023-08-10 10:49:37 +0500

    Tailwind CSS used to modify UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-// DataComponent.js
+
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchData } from '../redux/dataSlice'; // Your data slice file
+import { fetchData } from '../redux/dataSlice';
+
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -20,18 +21,29 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h1>Data from API</h1>
-      <ul>
-        {data.articles.map((item) => (
-          <li>
-            {item.title}
-            <h1>{item.author}</h1>
-           
-            </li>
+    <div className='bg-gray-100 min-h-screen'>
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='text-4xl font-bold mb-4 mt-16 text-center'>Latest News</h1>
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        {data.articles.map((item, index) => (
+          <div
+            key={index}
+            className='bg-white rounded shadow-md'
+          >
+            <img src={item.urlToImage} alt={item.title} className='w-full h-32 object-cover rounded-t' />
+            <div className='p-4'>
+              <p className='text-xl font-bold text-gray-800 mb-2'>{item.title}</p>
+              <p className='text-gray-600'>{item.author}</p>
+              <button className='bg-blue-600 text-white py-2 px-4 mt-4 rounded-md hover:bg-blue-600 hover:shadow-lg transition duration-300'>
+  Read More
+</button>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
+  </div>
+  
   );
 };
 
